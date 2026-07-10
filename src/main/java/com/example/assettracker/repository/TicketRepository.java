167c9e03fd02be26426b2d69da741b4c7ee06db2
DanialBrah <danialx003@gmail.com>
2026-07-10 14:47:36 +0800

    exercise 2

---
+++ src/main/java/com/example/assettracker/repository/TicketRepository.java
@@ -0,0 +1,10 @@
+package com.example.assettracker.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.assettracker.model.Ticket;
+
+public interface TicketRepository extends MongoRepository<Ticket, String> {
+    // Custom query methods can be defined here if needed
+
+}
